98edcf8381a3efbef64a5c12cd9829de31986b5d
Vansh810 <vansh.agarwal@gmail.com>
2024-05-18 18:09:28 +0530

    Added Linear Regression, Random Forest and XGBoost Models for prediction

--- HousePrice.py
+++ HousePrice.py
@@ -1,9 +1,13 @@
 import pandas as pd
 import numpy as np
+import xgboost as xgb
 import seaborn as sns
-from matplotlib import pyplot as plt
+import matplotlib.pyplot as plt
 from scipy.stats import zscore
 from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error, r2_score
 
 dataset_path = 'Housing.csv'
 
@@ -51,6 +55,9 @@ for col in df.select_dtypes(include=np.number).columns:
     plt.title(f'Histogram of {col}')
     plt.show()
 
+# Applying Z score normalization to all numeric attributes
+df[numeric] = df[numeric].apply(zscore)
+
 # Split data into features and target
 X = df.drop('price', axis=1)
 y = df['price']
@@ -59,4 +66,32 @@ y = df['price']
 X = pd.get_dummies(X, drop_first=True)
 
 # Split data into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
+# Training Linear Regression Model
+reg = LinearRegression()
+reg.fit(X_train, y_train)
+y_pred_reg = reg.predict(X_test)
+r2_reg = r2_score(y_test, y_pred_reg)
+print(f"\nLinear Regression Metrics: ")
+print(f"RMSE: {np.sqrt(mean_squared_error(y_test, y_pred_reg))}")
+print(f"R^2: {r2_reg}")
+
+# Training Random Forest Model
+forest = RandomForestRegressor(n_estimators=500, random_state=0, criterion="friedman_mse", min_samples_split=5)
+forest.fit(X_train, y_train)
+y_pred_forest = forest.predict(X_test)
+r2_forest = r2_score(y_test, y_pred_forest)
+print(f"\nRandom Forest Metrics: ")
+print(f"RMSE: {np.sqrt(mean_squared_error(y_test, y_pred_forest))}")
+print(f"R^2: {r2_forest}")
+
+# XGBoost Regressor Model
+xgbr = xgb.XGBRegressor(n_estimators=1000, random_state=0, learning_rate=0.005)
+xgbr.fit(X_train, y_train)
+y_pred_xgbr = xgbr.predict(X_test)
+r2_xgbr = r2_score(y_test, y_pred_xgbr)
+print(f"\nXGBoost Metrics: ")
+print(f"RMSE: {np.sqrt(mean_squared_error(y_test, y_pred_xgbr))}")
+print(f"R^2: {r2_xgbr}")
+
